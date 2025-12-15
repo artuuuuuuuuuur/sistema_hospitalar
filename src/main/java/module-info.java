26993e7b51ce20cdf6cfca5683ed2ce80f7283aa
nandobru13 <fbrunofa8@gmail.com>
2025-12-15 20:46:32 -0300

    inicio da interface

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.uece.poo.sistema_hospitalar {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.uece.poo.sistema_hospitalar to javafx.fxml;
-    exports com.uece.poo.sistema_hospitalar;
-}
